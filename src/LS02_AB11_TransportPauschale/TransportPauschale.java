74bc3f81596cf210941cfe2ef93aacee140bdd56
diyar <diyar.mohammed@safety-doors.com>
2025-12-15 11:16:10 +0100

    .

--- src/LS02_AB11_TransportPauschale/TransportPauschale.java
+++ src/LS02_AB11_TransportPauschale/TransportPauschale.java
@@ -1,8 +1,4 @@
 package LS02_AB11_TransportPauschale;
 
 public class TransportPauschale {
-    public static void main(String[] args) {
-        System.out.println("hallo");
-        System.out.println("was geht");
-    }
 }
